ed4c5153e448d751baea2157fdbc9dfd963a3973
Kostiantyn Teltov <dneprokos@gmail.com>
2024-02-04 17:29:11 +0100

    Added python comparison perators section4

---
+++ src/python_bootcamp/section4_python_comparison_operators/__init__.py
@@ -0,0 +1,10 @@
+"""
+| Operator | Description                | Example        |
+|----------|----------------------------|----------------|
+| ==       | Equal to                   | a == b         |
+| !=       | Not equal to               | a != b         |
+| >        | Greater than               | a > b          |
+| <        | Less than                  | a < b          |
+| >=       | Greater than or equal to   | a >= b         |
+| <=       | Less than or equal to      | a <= b         |
+"""
